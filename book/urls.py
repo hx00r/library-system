a976022820a4b1020695a44e7a7b432675818501
Hx00r <hx00rr@gmail.com>
2024-10-08 21:04:12 +0300

    added books routes

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .api import views as booksApiViews
 
 urlpatterns = [
-    path('books/', booksApiViews.BooksViews.as_view()),
-    path('books/<int:id>/', booksApiViews.BooksViews.as_view(), name='booksViews'),
+    path('books/', booksApiViews.BooksViews.as_view(), name="booksViews"),
+    path('books/<int:id>', booksApiViews.BooksViews.as_view(), name='booksViewsArgs'),
 ]
